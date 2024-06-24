58a638b8e263f4b0d699689edfdc1c41ec6fc471
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-06-24 11:44:16 +0100

    task: Added root cmd

---
+++ cmd/root.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "httpace-cli",
+	Short: "httpace-cli is a tool for making HTTP requests",
+	Long:  `A Fast and Flexible HTTP CLI tool built with love in Go.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
